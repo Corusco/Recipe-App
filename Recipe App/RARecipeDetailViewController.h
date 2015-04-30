8bb0f28d6f0a1a0c6f15169b1a634bdfe257a8ef
Justin Huntington <corusco@gmail.com>
2015-04-30 13:15:21 -0600

    Finished Through Step 5.

---
+++ Recipe App/RARecipeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RARecipeDetailViewController.h
+//  Recipe App
+//
+//  Created by Justin Huntington on 4/30/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RARecipeDetailViewController : UIViewController
+
+@end
