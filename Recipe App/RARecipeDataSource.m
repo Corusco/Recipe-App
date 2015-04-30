3d61645e2392d6fb140e8e489ebbb2ef025b2cf0
Justin Huntington <corusco@gmail.com>
2015-04-30 09:15:50 -0600

    practice. ended somewhere in step 4.

---
+++ Recipe App/RARecipeDataSource.m
@@ -0,0 +1,13 @@
+//
+//  RARecipeDataSource.m
+//  Recipe App
+//
+//  Created by Justin Huntington on 4/30/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "RARecipeDataSource.h"
+
+@implementation RARecipeDataSource
+
+@end
---
+++ Recipe App/RAViewController.h
@@ -0,0 +1,13 @@
+//
+//  RAViewController.h
+//  Recipe App
+//
+//  Created by Justin Huntington on 4/30/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAViewController : UIViewController
+
+@end
